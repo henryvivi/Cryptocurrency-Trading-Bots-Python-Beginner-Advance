1ff273cdada289f7f2529e2ae146b10f70620b73
henryvivi <henryyowise@gmail.com>
2024-08-05 11:59:39 +0100

    Update BinanceKeys.py

--- BinanceKeys.py
+++ BinanceKeys.py
@@ -1,3 +1,8 @@
-BinanceKey1 = {'api_key': 'YourApiKey',
-    'api_secret':'YourApiSecret'}
-	
+BinanceKey1={'api_key':BqODjyySx5CDC4LFHm6EAC65R4Ur5xPteC3M9HWbI¡A1MAjnRKVc01SGaFDy9Xmr 'YourApiKey'
+    'api_secret':RD259TurdwRDJ9wZ1HGtaOmC42QYR3w20bjcJXGh8p2myePew2W8bRAdBxWVQ00g 'YourApiSecret'}
+Kucoin2={'api_key':66ae57428447cd0001e7518d 'YourApiKey'
+    'api_secret':cd9fb871-bc74-4d02-a55e-302da0e90fc5 'YourApiSecret'}
+BybitKey3={'api_key':iFOitd75exvNIsvtAn 'YourApiKey'
+    'api_secret':FdxHWMgQgjCvqDKNJOxvU1rJKRRPZtFSqk7s 'YourApiSecret'}
+Okx4={'api_key':ec0f5aa6-117e-41f8-8725-8d9a8575d61f 'YourApiKey'
+    'api_secret':4DF39A91A148900C4E7DD239BF45C59C 'YourApiSecret'}
